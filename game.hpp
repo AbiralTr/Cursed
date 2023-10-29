07e0bfd08d80886e91263591f9ac584807366ce1
AbiralTr <abiraltuladhar22@gmail.com>
2023-10-29 13:27:58 -0600

    Created and Shifted Entity header and class

---
+++ game.hpp
@@ -0,0 +1,33 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+class Game{
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+
+private:
+};
+
+void runSDL(){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Cursed - Version 0.0.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+}
+
+int main(int argv, char** args){
+    runSDL();
+
+    return 0;
+}
